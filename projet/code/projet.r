a44491cc2e89d9ff163b2f9c8492b604b87b3e63
frochet <oversnals@hotmail.com>
2013-11-13 12:01:14 +0100

    func

--- projet/code/projet.r
+++ projet/code/projet.r
@@ -12,3 +12,23 @@ bw <- s[1]
 bxl <- s[2]
 fl <- s[3]
 
+
+
+#
+# data is a clean matrix where all of the elements are meaningful
+# for the statistical measurement
+#
+stat_descr <- function(data, file_to_save){
+  #todo : improve the function to hava generic col name => synthax to search
+  #over col names ? 
+  summary_T = summary(data$T)
+  summary_AGE = summary(data$AGE)
+  sd_T = sd(data$T)
+  sd_AGE = sd(data$AGE)
+
+  data_to_write <- matrix(c(summary_T, sd_T), c(summary_AGE, sd_AGE))
+  rownames(data_to_write) <- c("Min", "Q25", "Moyenne", "Medianne","Q75",
+			       "Max", "Ecart-type")
+  colnames(data_to_write) <- c("T","AGE")
+  write.table(data_to_write, file_to_save)
+}
